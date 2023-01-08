7144236e392b561345f20e6fc87042906b66a8ad
Abdul Basit <72595547+AbdulBasit098@users.noreply.github.com>
2023-01-08 00:22:40 +0000

    Add files via upload

---
+++ mapreduce.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class FashionDataBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_data,
+                   reducer=self.reducer_count_data)
+        ]
+
+    def mapper_get_data(self, _, line):
+        (brand_id,price,colour) = line.split('\t')
+        yield price, 1
+
+    def reducer_count_data(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    FashionDataBreakdown.run()
